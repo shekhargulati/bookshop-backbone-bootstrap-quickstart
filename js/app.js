b13dd5063b516e527e2d44a0eeedaf4d853a0d03
Shekhar Gulati <shekhargulati84@gmail.com>
2013-02-22 06:59:03 +0530

    router added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 // app.js
+
+(function($){
+
+	var Bookshop = {};
+	window.Bookshop = Bookshop;
+
+	Bookshop.Router = Backbone.Router.extend({
+		initialize : function(options){
+			console.log('In initialize()....');
+			this.el = options.el;
+		},
+		routes : {
+			"" : "index",
+		},
+		index : function(){
+			console.log('in index()....');
+			this.el.empty();
+		}
+	});
+
+	var router = new Bookshop.Router({el : $($('#main'))});
+	Backbone.history.start();
+	
+})(jQuery);
